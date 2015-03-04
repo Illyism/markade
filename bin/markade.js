8e94bfddfff08349fc7831defa92e90f4c642f4c
Ilias Ismanalijev <hello@illyism.com>
2015-03-04 11:12:42 +0100

    correct versioning

--- bin/markade.js
+++ bin/markade.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var pkg = require("../package.json");
  
 program
-  .version('0.1.0')
+  .version(pkg.version)
 
 program
   .command('init [directory]')
